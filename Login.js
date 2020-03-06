119dc7b38c4c4ce139afbf061ba8f5b0612df659
britt-c <bhitchman92@gmail.com>
2020-03-06 14:37:27 -0800

    created little form for the sign-in

--- Login.js
+++ Login.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 const Login = (props) => {
 	return (
-
+		<div>
+			<form>
+				<label for="email">Email</label>
+				<input type="email" id="email" name="email" />
+				<br />
+				<label for="password">Password</label>
+				<input type="password" id="pwd" name="pwd" />
+				<br />
+				<input type="button">
+			</form>
+		</div>
 	)}
 export default Login
